6a424f1467f81254925d20f2ac066eaebe639219
phatmenghor <phatmenghor19@gmail.com>
2024-12-15 14:13:27 +0700

    01

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
-import { keyStorage } from "./constants/storage/key_storage";
-import { routed } from "./constants/navigation/routed";
+// // middleware.ts
+// import { NextRequest, NextResponse } from "next/server";
+// import { keyStorage } from "./constants/storage/key_storage";
+// import { routed } from "./constants/navigation/routed";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get(keyStorage.TOKEN);
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get(keyStorage.TOKEN);
 
-  // if (request.nextUrl.pathname.startsWith("/" + routed.login) && !token) {
-  //   return NextResponse.next();
-  // }
+//   // if (request.nextUrl.pathname.startsWith("/" + routed.login) && !token) {
+//   //   return NextResponse.next();
+//   // }
 
-  // if (!token) {
-  //   return NextResponse.redirect(new URL("/" + routed.login, request.url));
-  // }
+//   // if (!token) {
+//   //   return NextResponse.redirect(new URL("/" + routed.login, request.url));
+//   // }
 
-  // if (request.url.includes("/login") && token) {
-  //   return NextResponse.next();
-  // }
+//   // if (request.url.includes("/login") && token) {
+//   //   return NextResponse.next();
+//   // }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
-};
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/"],
+// };
