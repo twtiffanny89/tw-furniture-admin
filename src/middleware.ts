8fceeb3fc47ee26f57fdb47911d53c9e368326c4
phatmenghor <phatmenghor19@gmail.com>
2024-12-15 14:11:01 +0700

    01

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,17 @@ import { routed } from "./constants/navigation/routed";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get(keyStorage.TOKEN);
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/" + routed.login, request.url));
-  }
+  // if (request.nextUrl.pathname.startsWith("/" + routed.login) && !token) {
+  //   return NextResponse.next();
+  // }
+
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/" + routed.login, request.url));
+  // }
+
+  // if (request.url.includes("/login") && token) {
+  //   return NextResponse.next();
+  // }
 
   return NextResponse.next();
 }
