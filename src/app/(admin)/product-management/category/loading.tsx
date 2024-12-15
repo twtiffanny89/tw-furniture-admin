30550a2fbd4832da115ab130d4d8409d16dd61b8
phatmenghor <phatmenghor19@gmail.com>
2024-12-15 14:25:02 +0700

    01

---
+++ src/app/(admin)/product-management/category/loading.tsx
@@ -0,0 +1,8 @@
+import CenteredLoading from "@/components/loading/center_loading";
+import React from "react";
+
+const loading = () => {
+  return <CenteredLoading />;
+};
+
+export default loading;
